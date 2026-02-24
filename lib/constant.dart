76f22710c0a394e46bb4bad00048bba522e114e6
youssef Belal <111551440+youssef336@users.noreply.github.com>
2026-02-24 22:19:59 +0200

    change color

--- lib/constant.dart
+++ lib/constant.dart
@@ -5,8 +5,8 @@ const KTopPadding = 16.0;
 
 const KprimaryColor = Color.fromARGB(255, 0, 81, 255);
 const KprimaryColorLight = Color.fromARGB(255, 85, 7, 7);
-const KprimaryColorDark = Color.fromARGB(255, 0, 81, 255);
-const KsecondaryColor = Color.fromARGB(255, 128, 128, 128);
+const KprimaryColorDark = Color.fromARGB(255, 5, 28, 79);
+const KsecondaryColor = Color.fromARGB(255, 129, 54, 54);
 
 const KisBoardingViewSeen = 'isBoardingViewSeen';
 
