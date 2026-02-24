7aa1f9ee20fbfeabad833250c222d930b3d9ab1e
youssef Belal <111551440+youssef336@users.noreply.github.com>
2026-02-24 22:21:12 +0200

    change color

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 const KhorzontalPadding = 16.0;
 const KTopPadding = 16.0;
 
-const KprimaryColor = Color.fromARGB(255, 0, 81, 255);
+const KprimaryColor = Color.fromARGB(255, 2, 5, 11);
 const KprimaryColorLight = Color.fromARGB(255, 85, 7, 7);
 const KprimaryColorDark = Color.fromARGB(255, 5, 28, 79);
 const KsecondaryColor = Color.fromARGB(255, 129, 54, 54);
