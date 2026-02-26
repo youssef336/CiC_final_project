12e152c1646a7ffbd706d88b40999704af724d36
youssef Belal <111551440+youssef336@users.noreply.github.com>
2026-02-26 16:18:12 +0200

    add

--- lib/features/home/presentation/views/widgets/product_grid_view_bloc_builder.dart
+++ lib/features/home/presentation/views/widgets/product_grid_view_bloc_builder.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'products_grid_view.dart';
+import 'package:mysterybag/constant.dart';
+import 'package:mysterybag/core/models/bag_item_model.dart';
+import 'package:mysterybag/core/models/restaurant_entity_model.dart';
+import 'package:mysterybag/core/widgets/available_bags_list.dart';
+import 'package:mysterybag/core/widgets/resturant_card.dart';
 
 class ProductGridViewBlocBuilder extends StatelessWidget {
   const ProductGridViewBlocBuilder({super.key});
